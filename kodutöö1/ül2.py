819a03ee33a592ce0408fedfce3e60747e01afc9
Kamazop <95763578+Kamazop@users.noreply.github.com>
2024-10-16 19:02:55 +0300

    Update ül2.py

--- kodutöö1/ül2.py
+++ kodutöö1/ül2.py
@@ -1,25 +1,26 @@
+# FIFO struktuur
 import array as massiiv
 
-x = massiiv.array("j", [])
+x = massiiv.array("i", []) # Tekitab tühja massiivi, kuhu hakatakse elemente lisama
 
 while True:
-  lisa = input("Kas soovid massiivi arvu lisada? yes/no:") # Küsib kasutajalt üle, kas ta soovib arvu lisada
-  if lisa.lower() == "yes":
-    try:
-      lisaint = int(input("Sisesta arv:")) # Kasutaja annab sisendi...
-      x.append(lisaint) # see lisatakse massiivi lõppu
-      print(f"Massiiv peale lisamist: {x}")
-    except ValueError:
-      print("Palun sisesta korrektne arv.")
-  else:
-    print(f"Praegune massiiv: {x}")
+    lisa = input("Kas soovid massiivi arvu lisada? jah/ei: ") # Küsib kasutajalt üle, kas ta soovib arvu lisada
+    if lisa.lower() == "jah":
+        try:
+            lisaint = int(input("Sisesta arv: "))  # Kasutaja annab sisendiks arvu
+            x.append(lisaint)  # see lisatakse massiivi lõppu
+            print(f"Massiiv peale lisamist: {x}")
+        except ValueError:
+            print("Palun sisesta korrektne arv.")
+    else:
+        print(f"Praegune massiiv: {x}")
 
-kustuta = input("Kas soovid massiivi esimesena lisatud arvu kustutada? yes/no:")
-if kustuta.lower == "yes":
-  if len(x) > 0:
-    print(f"Kustutatud arv: {x.pop(0)}") # Kustutab esimesena lisatud elemendi
-    print(f"Massiiv peale kustutamist: {x}")
-  else:
-    print("Massiiv on tühi, ei saa kustutada")
-else:
-  print(f"Praegune massiiv: {x}")
+    kustuta = input("Kas soovid massiivist esimest lisatud arvu kustutada? jah/ei: ") # Küsib kasutajalt, kas soovib esimesena lisatud arvu kustutada
+    if kustuta.lower() == "jah":
+        if len(x) > 0:
+            print(f"Kustutatud arv: {x.pop(0)}")  # Kustutab esimesena lisatud elemendi
+            print(f"Massiiv peale kustutamist: {x}")
+        else:
+            print("Massiiv on tühi, ei saa kustutada.")
+    else:
+        print(f"Praegune massiiv: {x}")

5396370bcb266923b4875eecfb020a826d5bb284
Kamazop <95763578+Kamazop@users.noreply.github.com>
2024-10-16 18:47:15 +0300

    Update ül2.py

--- kodutöö1/ül2.py
+++ kodutöö1/ül2.py
@@ -3,12 +3,22 @@ import array as massiiv
 x = massiiv.array("j", [])
 
 while True:
-  lisa = input("Kas soovid massiivi arvu lisada? yes/no:")
+  lisa = input("Kas soovid massiivi arvu lisada? yes/no:") # Küsib kasutajalt üle, kas ta soovib arvu lisada
   if lisa.lower() == "yes":
     try:
-      lisaint = in(input("Sisesta arv:"))
-      x.append(lisaint)
-      print("Massiiv peale lisamist: {x}")
+      lisaint = in(input("Sisesta arv:")) # Kasutaja annab sisendi...
+      x.append(lisaint) # see lisatakse massiivi lõppu
+      print(f"Massiiv peale lisamist: {x}")
 
 else:
-  print("Praegune massiiv: {x}")
+  print(f"Praegune massiiv: {x}")
+
+kustuta = input("Kas soovid massiivi esimesena lisatud arvu kustutada? yes/no:")
+if kustuta.lower == "yes":
+  if len(x) > 0:
+    print(f"Kustutatud arv: {x.pop(0)}") # Kustutab esimesena lisatud elemendi
+    print(f"Massiiv peale kustutamist: {x}")
+  else:
+    print("Massiiv on tühi, ei saa kustutada")
+else:
+  print(f"Praegune massiiv: {x}")

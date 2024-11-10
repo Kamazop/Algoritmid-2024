b9461276aef84798431b46347c2895124c4e8183
Kamazop <95763578+Kamazop@users.noreply.github.com>
2024-11-10 16:50:20 +0200

    Create ül2.py
    Binary search

---
+++ kodutöö3/ül2.py
@@ -0,0 +1,4 @@
+# Binari search
+# Kirjutage programm, mis teostab Binary Search'i sorteeritud täisarvude massiivil.
+# 2. Võrrelge teoreetilises analüüsis valminud Binary Search'i ja Linear Search'i aegkomplekssust.
+# 3. Tooge näide stsenaariumist, kus Binary Search on kasulikum kui Linear Search, ja selgitage miks.

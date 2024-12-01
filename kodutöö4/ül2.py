e231c1afff52a968e3910ebca497d53ac3ff7261
Kamazop <95763578+Kamazop@users.noreply.github.com>
2024-12-01 19:13:22 +0200

    Update ül2.py

--- kodutöö4/ül2.py
+++ kodutöö4/ül2.py
@@ -16,3 +16,16 @@ insert(index_map, 7)
 # Kontrollime olemasolu
 print(exists(index_map, 3))  # True
 print(exists(index_map, 5))  # False
+
+# AJakompleksus - väga efektiivne
+#    Lisamine O(1)
+#    Kontroll O(1)
+
+# Ruumikompleksus
+#    Mälu kasutus O(U), kus U on väärtuste ulatus.
+#    Kui väärtuste ulatus on suur ja enamik väärtusi puudub, kulub mälu ebaefektiivselt.
+
+# Kasutusvõimalused reaalses maailmas:
+# 1. Andmete olemasolu kiire kontrollimine - Näiteks kui on vaja kindlaks teha, kas üks elemnt eksisteerib kogumis (ID number või failinimi).
+# 2. Statistika kogumine - Näiteks täisarvude sageduste arvutamisel saab kasutada massiivi, kus iga indeksi väärtus vastab sagedusele.
+# 3. Geomeetria ja graafika - 2D-kaartides või pixel-massiivides, kus iga ruudu väärtus tähistab kindlat omadust.
